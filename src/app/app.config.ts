918642402319f6c5b19fd5704dbe86a3ab0b5660
narinderdev <narinder.dev@gmail.com>
2025-12-30 14:31:00 +0530

    Automaic signout if token is invalid or expired

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,16 +8,17 @@ import {
   provideClientHydration,
   withEventReplay
 } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { authErrorInterceptor } from './interceptors/auth-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([authErrorInterceptor])),
     provideAnimations(),
     provideToastr({
       positionClass: 'toast-top-right',
