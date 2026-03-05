308d42e45b5e63f9bad6e10898569de69cc94d29
narinderdev <narinder.dev@gmail.com>
2026-03-05 18:26:32 +0530

    Updated base URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://3.84.76.210:8080'
+  apiUrl: 'http://3.82.49.182:8080',
+    // apiUrl: 'https://a701-203-190-154-162.ngrok-free.app'
+
 };
